369ce561d28baf375b3606415832c45563c3ced4
Lenon Yuri <lenonyurisilva26@gmail.com>
2024-08-08 12:47:21 -0300

    08/08

--- src/main.cpp
+++ src/main.cpp
@@ -1,195 +1,102 @@
-#include <ESP8266WiFi.h>          // Inclui a biblioteca para funcionalidades WiFi no ESP8266
-#include <ESPAsyncWebServer.h>    // Inclui a biblioteca para criar um servidor web assíncrono
-#include <LittleFS.h>             // Inclui a biblioteca para o sistema de arquivos LittleFS
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
 
-// -------------------------------------------------------------------------
-// Cabeçalhos Personalizados
-// -------------------------------------------------------------------------
-#include "index.h"           // Cabeçalho para a configuração da página inicial
-#include "autenticador.h"    // Cabeçalho para a autenticação
-#include "dashboard.h"       // Cabeçalho para a página do dashboard
-#include "ligadesliga.h"     // Cabeçalho para as funções de ligar e desligar
-#include "creditos.h"        // Cabeçalho para a página de créditos
-#include "wificonexao.h"     // Cabeçalho para a configuração WiFi
-#include "paginaserro.h"     // Cabeçalho para as páginas de erro
-#include "wifigerenciador.h" // Cabeçalho para o gerenciamento do WiFi
+#include "index.h"
+#include "autenticador.h"
+#include "dashboard.h"
+#include "ligadesliga.h"
+#include "creditos.h"
+#include "wificonexao.h"
+#include "paginaserro.h"
+#include "wifigerenciador.h"
 
-// -------------------------------------------------------------------------
-// Configuração do Servidor Web
-// -------------------------------------------------------------------------
-AsyncWebServer server(80); // Criação do objeto do servidor web na porta 80 (HTTP)
+AsyncWebServer server(80);
 
-// -------------------------------------------------------------------------
-// Declarações de Funções
-// -------------------------------------------------------------------------
-void setupLittleFS();                                          // Função para inicializar o sistema de arquivos LittleFS
-void setupServer();                                          // Função para configurar o servidor e suas rotas
-void configureRoutes();                                      // Função para configurar as rotas do servidor
-bool isAuthenticated(AsyncWebServerRequest *request);        // Função para verificar se a requisição está autenticada
-void redirectToAccessDenied(AsyncWebServerRequest *request); // Função para redirecionar para a página de acesso negado
+void setupLittleFS();
+void setupServer();
+bool isAuthenticated(AsyncWebServerRequest *request);
+void redirectToAccessDenied(AsyncWebServerRequest *request);
 
-// -------------------------------------------------------------------------
-// Variáveis de Controle de Reconexão WiFi
-// -------------------------------------------------------------------------
-unsigned long lastReconnectAttempt = 0; // Armazena o tempo da última tentativa de reconexão
-int reconnectAttempts = 0;              // Contador de tentativas de reconexão
+const unsigned long RECONNECT_INTERVAL = 1000;
+const int MAX_RECONNECT_ATTEMPTS = 10;
 
-const int MAX_RECONNECT_ATTEMPTS = 10; // Número máximo de tentativas de reconexão
+unsigned long lastReconnectAttempt = 0;
+int reconnectAttempts = 0;
 
-// Definição dos intervalos
-const unsigned long RECONNECT_INTERVAL = 1000; // Intervalo para reconexão WiFi (1 segundo)
-const unsigned long UPDATE_INTERVAL = 300000;  // Intervalo para atualização do tempo e status do compressor (5 minutos)
-const unsigned long RESTART_TIME = 60000;      // Tempo para aguardar após verificar horário de reinicialização (1 minuto)
-
-// Variáveis para controle de tempo
-unsigned long lastUpdate = 0;
-unsigned long lastCompressorUpdate = 0;
-
-// -------------------------------------------------------------------------
-// Função de Configuração Inicial
-// -------------------------------------------------------------------------
-void setup()
-{
-    // Inicia a comunicação serial para depuração
+void setup() {
     Serial.begin(115200);
-    Serial.println("Iniciando o setup..."); // Mensagem indicando o início do setup
 
-    // Configura o ESP8266 para o modo Station (cliente) e aguarda um pouco
-    WiFi.mode(WIFI_STA); // Define o modo WiFi como Station
-    delay(1000);         // Aguarda 1 segundo
+    WiFi.mode(WIFI_STA);
+    delay(1000);
 
-    // Inicializa o sistema de arquivos LittleFS e tenta conectar às redes salvas
-    setupLittleFS();           // Chama a função para inicializar o LittleFS
-    loadSavedWiFiNetworks();  // Função para carregar redes WiFi salvas (não definida no código fornecido)
+    setupLittleFS();
+    loadSavedWiFiNetworks();
 
-    // Configura o servidor web e inicializa o cliente NTP
-    setupServer(); // Chama a função para configurar o servidor e suas rotas
+    setupServer();
 }
 
-// -------------------------------------------------------------------------
-// Função de Loop Principal
-// -------------------------------------------------------------------------
-void loop()
-{
-    unsigned long currentMillis = millis();
-
-    // Verifica se o dispositivo está em modo AP e se não está, executa a reconexão WiFi
-    if (!isAPMode)
-    { // Verifica se o dispositivo não está em modo AP
-        if (WiFi.status() != WL_CONNECTED)
-        { // Se o status do WiFi não for conectado
-            if (currentMillis - lastReconnectAttempt >= RECONNECT_INTERVAL)
-            {                                                                   // Verifica se o intervalo de reconexão foi alcançado
-                lastReconnectAttempt = currentMillis;                           // Atualiza o tempo da última tentativa de reconexão
-                Serial.println("Conexão WiFi perdida. Tentando reconectar..."); // Mensagem indicando tentativa de reconexão
-
-                // Tenta reconectar à rede WiFi
-                connectToWiFi(ssid, password); // Função para conectar ao WiFi
-                reconnectAttempts++;           // Incrementa o contador de tentativas
-                if (reconnectAttempts >= MAX_RECONNECT_ATTEMPTS)
-                { // Verifica se o número máximo de tentativas foi alcançado
-                    Serial.println("Número máximo de tentativas de reconexão alcançado. Entrando em modo AP.");
-                    enterAPMode();         // Função para entrar em modo Access Point
-                    reconnectAttempts = 0; // Reseta o contador de tentativas após entrar no modo AP
-                }
+void loop() {
+    if (!isAPMode && WiFi.status() != WL_CONNECTED) {
+        unsigned long currentMillis = millis();
+        if (currentMillis - lastReconnectAttempt >= RECONNECT_INTERVAL) {
+            lastReconnectAttempt = currentMillis;
+            connectToWiFi(ssid, password);
+            reconnectAttempts++;
+            if (reconnectAttempts >= MAX_RECONNECT_ATTEMPTS) {
+                enterAPMode();
+                reconnectAttempts = 0;
             }
         }
     }
-    else
-    {
-        reconnectAttempts = 0; // Reseta o contador de tentativas quando estiver em modo AP
+}
+
+void setupLittleFS() {
+    if (!LittleFS.begin()) {
+        Serial.println("Erro ao montar o sistema de arquivos");
     }
 }
 
-// -------------------------------------------------------------------------
-// Funções Auxiliares
-// -------------------------------------------------------------------------
-/**
- * Inicializa o sistema de arquivos LittleFS.
- */
-void setupLittleFS()
-{
-    if (!LittleFS.begin()) // Tenta iniciar o LittleFS
-    {
-        Serial.println("Falha ao iniciar o sistema de arquivos LittleFS"); // Mensagem de erro se o LittleFS não iniciar
-        return;                                                           // Retorna da função se a inicialização falhar
-    }
-    Serial.println("LittleFS inicializado com sucesso."); // Mensagem de sucesso se o LittleFS iniciar corretamente
+void setupServer() {
+    server.on("/login", HTTP_POST, [](AsyncWebServerRequest *request) {
+        handleLogin(request);
+    });
+
+    server.on("/logout", HTTP_GET, [](AsyncWebServerRequest *request) {
+        handleLogout(request);
+    });
+
+    server.on("/dashboard", HTTP_GET, [](AsyncWebServerRequest *request) {
+        if (isAuthenticated(request)) {
+            request->send(LittleFS, "/dashboard.html", "text/html");
+        } else {
+            redirectToAccessDenied(request);
+        }
+    });
+
+    server.on("/toggle", HTTP_ANY, [](AsyncWebServerRequest *request) {
+        if (isAuthenticated(request)) {
+            handleToggleAction(server);
+        } else {
+            redirectToAccessDenied(request);
+        }
+    });
+
+    server.on("/check-auth", HTTP_GET, [](AsyncWebServerRequest *request) {
+        if (isAuthenticated(request)) {
+            request->send(200, "application/json", "{\"authenticated\":true}");
+        } else {
+            request->send(200, "application/json", "{\"authenticated\":false}");
+        }
+    });
+
+    server.onNotFound([](AsyncWebServerRequest *request) {
+        request->send(404, "text/plain", "Not found");
+    });
+
+    server.begin();
 }
 
-/**
- * Configura o servidor e suas rotas.
- */
-void setupServer()
-{
-    Serial.println("Configurando o servidor..."); // Mensagem indicando o início da configuração do servidor
-
-    // Configura as páginas e rotas do servidor
-    setupIndexPage(server);                // Configura a página inicial
-    setupCreditosPage(server);             // Configura a página de créditos
-    setupDashboardPage(server);            // Configura a página do dashboard
-    setupLigaDesliga(server);              // Configura as funções de ligar e desligar
-    setupAcessoInvalidoPage(server);       // Configura a página de acesso inválido
-    setupNotFoundPage(server);             // Configura a página de não encontrado
-    setupUsuarioJaLogadoPage(server);      // Configura a página de usuário já logado
-    setupCredenciaisInvalidasPage(server); // Configura a página de credenciais inválidas
-    setupWiFiManagementPage(server);       // Configura a página de gerenciamento WiFi
-
-    // Configura as rotas protegidas e públicas
-    configureRoutes(); // Chama a função para configurar as rotas
-
-    // Inicia o servidor web
-    server.begin();                      // Inicia o servidor web
-    Serial.println("Servidor iniciado"); // Mensagem indicando que o servidor foi iniciado
-}
-
-/**
- * Configura as rotas do servidor web.
- */
-void configureRoutes()
-{
-    // Rota de login
-    server.on("/login", HTTP_POST, [](AsyncWebServerRequest *request)
-    { handleLogin(request); }); // Função para lidar com a requisição de login
-
-    // Rota de logout
-    server.on("/logout", HTTP_GET, [](AsyncWebServerRequest *request)
-    { handleLogout(request); }); // Função para lidar com a requisição de logout
-
-    // Rota protegida: Dashboard
-    server.on("/dashboard", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        if (isAuthenticated(request)) { // Verifica se a requisição está autenticada
-            request->send(LittleFS, "/dashboard", "text/html"); // Envia a página do dashboard
-        } else {
-            redirectToAccessDenied(request); // Redireciona para a página de acesso negado
-        } });
-
-    // Rota protegida: Toggle
-    server.on("/toggle", HTTP_ANY, [](AsyncWebServerRequest *request)
-    {
-        if (isAuthenticated(request)) { // Verifica se a requisição está autenticada
-            handleToggleAction(server); // Função para lidar com a ação de toggle
-        } else {
-            redirectToAccessDenied(request); // Redireciona para a página de acesso negado
-        } });
-
-    // Rota para verificar autenticação
-    server.on("/check-auth", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        if (isAuthenticated(request)) { // Verifica se a requisição está autenticada
-            request->send(200, "application/json", "{\"authenticated\":true}"); // Responde com JSON indicando autenticação bem-sucedida
-        } else {
-            request->send(200, "application/json", "{\"authenticated\":false}"); // Responde com JSON indicando autenticação falha
-        } });
-}
-
-/**
- * Redireciona para a página de acesso inválido.
- *
- * @param request A requisição que resultou em acesso não autorizado.
- */
-void redirectToAccessDenied(AsyncWebServerRequest *request)
-{
-    request->redirect("/acesso-invalido"); // Redireciona para a página de acesso inválido
+void redirectToAccessDenied(AsyncWebServerRequest *request) {
+    request->redirect("/acesso-invalido.html");
 }
--- src/wificonexao.cpp
+++ src/wificonexao.cpp
@@ -6,182 +6,86 @@
 // -------------------------------------------------------------------------
 // Configurações Globais
 // -------------------------------------------------------------------------
-const char *ssid = "";                 // SSID da rede Wi-Fi para conexão
-const char *password = "";             // Senha da rede Wi-Fi para conexão
-const char *ap_ssid = "CompressorWeb"; // SSID do Access Point
-const char *ap_password = "12345678";  // Senha do Access Point
+const char *ssid = "";
+const char *password = "";
+const char *ap_ssid = "CompressorWeb";
+const char *ap_password = "12345678";
 
 // Configurações do Access Point
-IPAddress local_ip(192, 168, 26, 7); // IP local do Access Point
-IPAddress gateway(192, 168, 26, 1);  // Gateway do Access Point (corrigido para estar na mesma sub-rede)
-IPAddress subnet(255, 255, 255, 0); // Máscara de sub-rede do Access Point
+IPAddress local_ip(192, 168, 26, 7);
+IPAddress gateway(192, 168, 26, 1);
+IPAddress subnet(255, 255, 255, 0);
 
-// Variáveis para o gerenciamento de escaneamento de redes Wi-Fi
-bool scanning = false;                   // Indica se o escaneamento está em andamento
-unsigned long lastScan = 0;              // Tempo do último escaneamento
-const unsigned long scanInterval = 5000; // Intervalo entre escaneamentos (em milissegundos)
+bool isAPMode = false;
 
-bool isAPMode = false; // Indica se o dispositivo está no modo Access Point
-bool scanDone = false; // Indica se o escaneamento foi concluído
-int scanResults = 0;   // Número de redes encontradas
-
-// -------------------------------------------------------------------------
-// Função para Conectar ao Wi-Fi
-// -------------------------------------------------------------------------
-void connectToWiFi(const char *ssid, const char *password)
-{
-    if (!ssid || !password || strlen(ssid) == 0 || strlen(password) == 0)
-    {
+void connectToWiFi(const char *ssid, const char *password) {
+    if (!ssid || !password || strlen(ssid) == 0 || strlen(password) == 0) {
         Serial.println("SSID ou senha inválidos.");
         enterAPMode();
         return;
     }
 
-    Serial.print("Tentando conectar ao WiFi: ");
-    Serial.println(ssid);
+    WiFi.begin(ssid, password);
 
-    WiFi.begin(ssid, password); // Inicia a conexão com o Wi-Fi
-
-    int attempts = 0;
-    const int maxAttempts = 5;
-
-    while (WiFi.status() != WL_CONNECTED && attempts < maxAttempts)
-    {
+    for (int attempts = 0; attempts < 5 && WiFi.status() != WL_CONNECTED; attempts++) {
         delay(1000);
         Serial.print(".");
-        attempts++;
     }
 
-    if (WiFi.status() == WL_CONNECTED)
-    {
+    if (WiFi.status() == WL_CONNECTED) {
         Serial.println();
         Serial.print("Conexão feita! Endereço IP: ");
         Serial.println(WiFi.localIP());
         isAPMode = false;
-    }
-    else
-    {
+    } else {
         Serial.println();
         Serial.println("Conexão falha.");
         enterAPMode();
     }
 }
 
-// -------------------------------------------------------------------------
-// Função para Entrar no Modo Access Point
-// -------------------------------------------------------------------------
-void enterAPMode()
-{
+void enterAPMode() {
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("Já conectado a uma rede Wi-Fi. Não é necessário entrar no modo AP.");
+        return;
+    }
+
     Serial.println("Entrando no modo Access Point...");
-    WiFi.mode(WIFI_AP);                           // Configura o ESP8266 para o modo Access Point
-    WiFi.softAPConfig(local_ip, gateway, subnet); // Configura o IP, gateway e máscara
-    WiFi.softAP(ap_ssid, ap_password);            // Inicia o Access Point com SSID e senha
+    WiFi.disconnect();
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(local_ip, gateway, subnet);
+    WiFi.softAP(ap_ssid, ap_password);
     Serial.print("Modo AP iniciado. Endereço IP: ");
-    Serial.println(WiFi.softAPIP()); // Imprime o IP do Access Point
+    Serial.println(WiFi.softAPIP());
     isAPMode = true;
 }
 
-// -------------------------------------------------------------------------
-// Função para Iniciar o Escaneamento de Redes Wi-Fi
-// -------------------------------------------------------------------------
-void startScanWiFiNetworks()
-{
-    if (!scanning && millis() - lastScan > scanInterval)
-    {
-        scanning = true;
-        WiFi.scanNetworks(true); // Inicia o escaneamento de redes Wi-Fi
-        lastScan = millis();
-    }
-}
-
-// -------------------------------------------------------------------------
-// Função para Obter os Resultados do Escaneamento
-// -------------------------------------------------------------------------
-String getScanResults()
-{
-    int n = WiFi.scanComplete(); // Obtém o número de redes encontradas
-    if (n == -2 || n == -1)
-    {
-        return "[]";
-    }
-    else if (n == 0)
-    {
-        return "[]";
-    }
-    else
-    {
-        String json = "[";
-        for (int i = 0; i < n; i++)
-        {
-            json += "{\"ssid\":\"" + WiFi.SSID(i) + "\"}";
-            if (i < n - 1)
-                json += ","; // Adiciona vírgula entre os itens, exceto no último
-        }
-        json += "]";
-        scanning = false;
-        return json;
-    }
-}
-
-// -------------------------------------------------------------------------
-// Função para Verificar se o Escaneamento foi Concluído
-// -------------------------------------------------------------------------
-void checkScanResults()
-{
-    if (WiFi.scanComplete() != WIFI_SCAN_RUNNING)
-    {
-        scanDone = true;
-        scanResults = WiFi.scanComplete(); // Armazena o número de redes encontradas
-        scanning = false;
-    }
-}
-
-// -------------------------------------------------------------------------
-// Função para Carregar Redes Wi-Fi Salvas e Tentar Conexão
-// -------------------------------------------------------------------------
-void loadSavedWiFiNetworks()
-{
+void loadSavedWiFiNetworks() {
     File file = LittleFS.open("/wifiredes.txt", "r");
-    if (!file)
-    {
+    if (!file) {
         Serial.println("Erro ao abrir o arquivo de redes Wi-Fi");
         return;
     }
 
-    String content = file.readString();
-    file.close();
+    char line[64];
+    while (file.available()) {
+        file.readBytesUntil('\n', line, sizeof(line) - 1);
+        line[file.position()] = '\0'; // Garante que a string esteja terminada em NULL
 
-    String ssid;
-    String password;
-    std::vector<String> networks;
+        char *commaPos = strchr(line, ',');
+        if (commaPos) {
+            *commaPos = '\0'; // Termina o SSID
+            const char *savedSsid = line;
+            const char *savedPassword = commaPos + 1;
 
-    size_t start = 0;  // Use size_t para compatibilidade com length()
-    size_t length = content.length();
-    while (start < length)
-    {
-        size_t end = content.indexOf('\n', start);
-        if (end == -1)
-        {
-            end = length;
-        }
-        String line = content.substring(start, end);
-        networks.push_back(line);
-        start = end + 1;
-    }
-
-    for (int i = networks.size() - 1; i >= 0; i--)
-    {
-        String line = networks[i];
-        int commaIndex = line.indexOf(',');
-        if (commaIndex != -1)
-        {
-            ssid = line.substring(0, commaIndex);
-            password = line.substring(commaIndex + 1);
-            connectToWiFi(ssid.c_str(), password.c_str());
-            if (WiFi.status() == WL_CONNECTED)
-            {
+            connectToWiFi(savedSsid, savedPassword);
+            if (WiFi.status() == WL_CONNECTED) {
+                file.close();
                 return;
             }
         }
     }
+
+    file.close();
+    enterAPMode();
 }
