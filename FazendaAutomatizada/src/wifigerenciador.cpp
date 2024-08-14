595fd6413a16b49d89845f8d88c0cff27d8b5408
Lenon <lenonyurisilva26@gmail.com>
2024-08-14 01:12:34 -0300

    14/08

--- FazendaAutomatizada/src/wifigerenciador.cpp
+++ FazendaAutomatizada/src/wifigerenciador.cpp
@@ -20,12 +20,6 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
               {
         Serial.println("Rota /listadewifi acessada.");
 
-        if (!LittleFS.exists("/wifiredes.txt")) {
-            Serial.println("Arquivo de redes Wi-Fi não encontrado.");
-            request->send(404, "text/plain", "Arquivo de redes Wi-Fi não encontrado.");
-            return;
-        }
-
         File file = LittleFS.open("/wifiredes.txt", "r");
         if (!file) {
             Serial.println("Erro ao abrir o arquivo de redes Wi-Fi.");
@@ -35,7 +29,6 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
 
         String networks = file.readString();
         file.close();
-        Serial.println("Redes Wi-Fi lidas com sucesso.");
         request->send(200, "text/plain", networks); });
 
     server.on("/salvarwifi", HTTP_POST, [](AsyncWebServerRequest *request)
@@ -45,21 +38,6 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
         if (request->hasParam("ssid", true) && request->hasParam("password", true)) {
             String ssid = request->getParam("ssid", true)->value();
             String password = request->getParam("password", true)->value();
-            Serial.print("SSID recebido: ");
-            Serial.println(ssid);
-            Serial.print("Password recebido: ");
-            Serial.println(password);
-
-            if (!LittleFS.exists("/wifiredes.txt")) {
-                Serial.println("Arquivo de redes Wi-Fi não encontrado. Criando novo arquivo.");
-                File file = LittleFS.open("/wifiredes.txt", "w");
-                if (!file) {
-                    Serial.println("Erro ao criar o arquivo de redes Wi-Fi.");
-                    request->send(500, "text/plain", "Erro ao criar o arquivo de redes Wi-Fi");
-                    return;
-                }
-                file.close();
-            }
 
             File file = LittleFS.open("/wifiredes.txt", "r");
             if (!file) {
@@ -84,8 +62,6 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
                     if (savedSSID == ssid) {
                         newContent += ssid + "," + password + "\n";
                         ssidExists = true;
-                        Serial.print("SSID existente atualizado: ");
-                        Serial.println(savedSSID);
                     } else {
                         newContent += line + "\n";
                     }
@@ -96,8 +72,6 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
             }
             if (!ssidExists) {
                 newContent += ssid + "," + password + "\n";
-                Serial.print("Novo SSID adicionado: ");
-                Serial.println(ssid);
             }
 
             file = LittleFS.open("/wifiredes.txt", "w");
@@ -109,12 +83,9 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
             file.print(newContent);
             file.close();
 
-            Serial.println("SSID e senha salvos com sucesso. Tentando conectar às redes salvas...");
             connectToSavedNetworks();
-
             request->redirect("/wifigerenciamento");
         } else {
-            Serial.println("Dados ausentes (SSID ou senha).");
             request->send(400, "text/plain", "Dados ausentes.");
         } });
 
@@ -124,19 +95,6 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
 
         if (request->hasParam("ssid")) {
             String ssidToDelete = request->getParam("ssid")->value();
-            Serial.print("SSID a ser excluído: ");
-            Serial.println(ssidToDelete);
-
-            if (!LittleFS.exists("/wifiredes.txt")) {
-                Serial.println("Arquivo de redes Wi-Fi não encontrado. Criando novo arquivo.");
-                File file = LittleFS.open("/wifiredes.txt", "w");
-                if (!file) {
-                    Serial.println("Erro ao criar o arquivo de redes Wi-Fi.");
-                    request->send(500, "text/plain", "Erro ao criar o arquivo de redes Wi-Fi");
-                    return;
-                }
-                file.close();
-            }
 
             File file = LittleFS.open("/wifiredes.txt", "r");
             if (!file) {
@@ -162,8 +120,6 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
                         newContent += line + "\n";
                     } else {
                         ssidFound = true;
-                        Serial.print("SSID encontrado e excluído: ");
-                        Serial.println(savedSSID);
                     }
                 } else {
                     newContent += line + "\n";
@@ -180,24 +136,18 @@ void setupWiFiGerenciamentoPage(AsyncWebServer &server)
                 }
                 file.print(newContent);
                 file.close();
-                Serial.println("SSID excluído com sucesso.");
                 request->redirect("/wifigerenciamento");
             } else {
-                Serial.println("SSID não encontrado no arquivo de redes Wi-Fi.");
                 request->send(404, "text/plain", "SSID não encontrado.");
             }
         } else {
-            Serial.println("SSID ausente na solicitação.");
             request->send(400, "text/plain", "SSID ausente.");
         } });
 
     server.on("/getip", HTTP_GET, [](AsyncWebServerRequest *request)
               {
         Serial.println("Rota /getip acessada.");
-        String ip = WiFi.localIP().toString();
-        Serial.print("IP Local: ");
-        Serial.println(ip);
-        request->send(200, "text/plain", ip); });
+        request->send(200, "text/plain", WiFi.localIP().toString()); });
 
     Serial.println("Rotas de gerenciamento Wi-Fi configuradas com sucesso.");
 }
